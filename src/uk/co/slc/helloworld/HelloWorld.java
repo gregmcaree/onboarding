f7b2552ea3f885a71ed3aa8aa8b9c1b6f8f7a3f6
gregmcaree <greg.mcaree369@gmal.com>
2014-06-17 10:47:02 +0100

    Local commit of Intelj tutorial - no compile errors

---
+++ src/uk/co/slc/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package uk.co.slc.helloworld;
+
+/**
+ * Created by dev on 12/06/14.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java World");
+    }
+}
